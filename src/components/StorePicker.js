d29a094d7255cec6114fe0cade60e984863ad6dd
khristos <khristos@myway.com>
2016-11-03 20:11:35 -0400

    Add JSX.  * 04 - Writing HTML with JSX.mp4

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
 	render() {
-		return <p>Hello</p>;
+		return (
+			<form className="store-selector">
+				{ /* Comments */ }
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store></button>
+			</form>
+		)
+		//return <p>Hello</p>;
 		//return React.createElement( 'p', {className: 'test'}, 'Hello again' );
 	}
 }
