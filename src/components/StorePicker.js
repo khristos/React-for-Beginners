2aea6142630b544ba41b8b33823910e50d9ef40c
khristos <khristos@myway.com>
2016-11-07 23:36:35 -0500

    11 - Working with React Events.mp4

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,18 +2,45 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	/*
+	constructor() {
+		super();
+		this.goToStore = this.goToStore.bind(this);
+	}
+	// Alternatively, use 'bind' method in the event handler, e.g. 'onSubmit'
+	*/
+	gotoStore(event){
+		event.preventDefault();
+		console.log('You changed the URL');
+		// firstly, grab text from text input
+		console.log(this.storeInput.value);
+		// secondly, transition from '/' to '/store/:storeId'
+	}
 	render() {
 		return (
-			<form className="store-selector">
-				{ /* Comments */ }
+			  <form className="store-selector" onSubmit={(e) => this.gotoStore(e)}> 
+			  { /* <form className="store-selector" onSubmit={this.gotoStore.bind(this)}> */ }
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-				<button type="submit">Visit Store></button>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input} } />
+				<button type="submit">Visit Store</button>
 			</form>
 		)
 		//return <p>Hello</p>;
 		//return React.createElement( 'p', {className: 'test'}, 'Hello again' );
 	}
 }
+
+
+// Old way. Not ES6.
+/*
+var storePicker = React.createClass({
+	goToStore() {
+		console.log(this);
+	},
+	render() {
+		return <p>hi</p>
+	}
+});
+*/
 
 export default StorePicker;
