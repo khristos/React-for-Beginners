dc289acf1164993291b3f202bb19a8c7c7ada5c9
khristos <khristos@myway.com>
2016-11-28 21:29:15 -0500

    16 - Updating Order State.mp4

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
 
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 
 		// Get initial state
 		this.state = {
@@ -21,13 +22,13 @@ class App extends React.Component {
 	}
 
 	addFish(fish) {
-		//update state
+		// update state (using 'object spread')
 		const fishes = {...this.state.fishes};
-		//add new fish
+		// add new fish
 		const timestamp = Date.now();
 		fishes[`fish-${timestamp}`] = fish;
 		//this.state.fishes.fish1 = fish;
-		//set state
+		// set state
 		this.setState({ fishes }); //shortcut
 		//this.setState({ fishes: fishes });
 	}
@@ -36,6 +37,16 @@ class App extends React.Component {
 		this.setState({
 			fishes: sampleFishes
 		});
+	}
+
+	addToOrder(key) {
+		// take a copy of state (using 'object spread')
+		const order = {...this.state.order};
+		// update or add the new number of fish ordered
+		order[key] = order[key] + 1 || 1;
+		// update state
+		//this.setState({ order: order });
+		this.setState({ order });
 	}
 
 	render() {
@@ -47,7 +58,7 @@ class App extends React.Component {
 						{
 							Object
 								.keys(this.state.fishes)
-								.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+								.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
 						}
 					</ul>
 				</div>
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,7 +4,11 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component {
 	render() {
 		//const details = this.props.details;
-		const { details } = this.props;
+		//const index = this.props.index;
+		const { details, index } = this.props; // ES6 destructuring
+		const isAvailable = details.status === 'available';
+		const buttonText = isAvailable ? 'Add To Order 🐟' : 'Sold Out!';
+
 		return (
 			<li className="menu-fish">
 				{ /* <img src={this.props.details.image} alt={this.props.details.name} /> */ }
@@ -14,7 +18,8 @@ class Fish extends React.Component {
 					<span className="price">{formatPrice(details.price)}</span>
 				</h3>
 				<p>{details.desc}</p>
-				<button>Add To Order 🐟</button>
+				{ /*<button onClick={this.props.addToOrder.bind(null,this.props.index)} disabled={!isAvailable}>{buttonText}</button>*/ }
+				<button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
 			</li>
 		)
 	}
