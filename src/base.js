cc40c34deb36a013ea23aba6877b9c226d6b9f5b
khristos <khristos@myway.com>
2016-12-01 18:38:12 -0500

    18 - Persisting our State with Firebase.mp4

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDXsnnilpynSFAB46iY0yYUctkt8oGbVM4",
+    authDomain: "catch-of-the-day-christos.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-christos.firebaseio.com"
+});
+
+export default base;
