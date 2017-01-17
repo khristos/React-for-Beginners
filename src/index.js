56f3fa7a74c48f7298233fa03249d6615be3f08c
khristos <khristos@myway.com>
2017-01-17 13:32:06 -0500

    minor formatting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 //import ReactDOM from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 
-import './css/style.css'
+import './css/style.css';
 
 import App from './components/App';
 import StorePicker from './components/StorePicker';
